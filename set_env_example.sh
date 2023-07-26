132644b95c78982e0a0b372771ac222e18471717
Cong Dinh <congdinh2012@hotmail.com>
2023-07-26 11:19:20 +0700

    git ignore set_env and add set_env_example.sh

--- set_env_example.sh
+++ set_env_example.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=aws
-export POSTGRES_PASSWORD=Abcd@1234
-export POSTGRES_HOST=localhost
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export POSTGRES_USERNAME=your_username
+export POSTGRES_PASSWORD=your_password
+export POSTGRES_HOST=your_rds_postgres_host
+export POSTGRES_DB=your_rds_postgres_db
+export AWS_BUCKET=your_s3_bucket_access_point_arn
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing

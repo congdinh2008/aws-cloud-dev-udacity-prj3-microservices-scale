9cd93b907e62278e3fe2eaf708dbb7017675a043
Cong Dinh <congdinh2012@hotmail.com>
2023-07-28 18:21:54 +0700

    Update deployment replica and tag v2 travis

--- command.sh
+++ command.sh
@@ -47,10 +47,10 @@ kubectl expose deployment reverseproxy --type=LoadBalancer --name=publicreversep
 base64 <<< testing
 
 # Redeploy 
-kubectl set image deployment backend-user backend-user=congdinh2012/udagram-api-user:latest
-kubectl set image deployment backend-feed backend-feed=congdinh2012/udagram-api-feed:latest
-kubectl set image deployment reverseproxy reverseproxy=congdinh2012/reverseproxy:latest 
-kubectl set image deployment udagram-frontend udagram-frontend=congdinh2012/udagram-frontend:latest
+kubectl set image deployment backend-user backend-user=congdinh2012/udagram-api-user:v2
+kubectl set image deployment backend-feed backend-feed=congdinh2012/udagram-api-feed:v2
+kubectl set image deployment reverseproxy reverseproxy=congdinh2012/reverseproxy:v2 
+kubectl set image deployment udagram-frontend udagram-frontend=congdinh2012/udagram-frontend:v2
 
 kubectl autoscale deployment backend-feed --cpu-percent=50 --min=1 --max=2
 kubectl autoscale deployment backend-user --cpu-percent=50 --min=1 --max=2

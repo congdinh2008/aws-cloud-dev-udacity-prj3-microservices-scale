62ae972eda9b7e1a878b5c991f904fabf50e725f
Cong Dinh <congdinh2012@hotmail.com>
2023-07-29 07:20:43 +0700

    Update enviroment for new build

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://afe3e6116c195442bba2c140059513cc-441421960.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://a0649a36000244ed38cbd320cfbc23cf-195589793.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*

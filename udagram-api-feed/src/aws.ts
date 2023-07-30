67e676a059a9d9f2ccf025b56dfd277c7e46ca31
Cong Dinh <congdinh2012@hotmail.com>
2023-07-30 19:16:31 +0700

    Update v3 with config.ts feed secret id and token

--- udagram-api-feed/src/aws.ts
+++ udagram-api-feed/src/aws.ts
@@ -5,6 +5,13 @@ import {config} from './config/config';
 // Configure AWS
 const credentials = new AWS.SharedIniFileCredentials({profile: config.aws_profile});
 AWS.config.credentials = credentials;
+console.log("before:", AWS.config.credentials);
+
+AWS.config.update({
+  accessKeyId: config.aws_access_key_id,
+  secretAccessKey: config.aws_secret_access_key
+});
+console.log("after", AWS.config.credentials);
 
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
@@ -27,9 +34,13 @@ export function getGetSignedUrl( key: string ): string {
 export function getPutSignedUrl( key: string ): string {
   const signedUrlExpireSeconds = 60 * 5;
 
-  return s3.getSignedUrl('putObject', {
+  const url = s3.getSignedUrl('putObject', {
     Bucket: config.aws_media_bucket,
     Key: key,
     Expires: signedUrlExpireSeconds,
   });
+
+  console.log("Signed URL: " + url);
+  
+  return url;
 }
